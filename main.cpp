78fdef35ba523f5ae6054adde7006d1d5673ece0
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-05-01 18:18:25 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	cout << "\n" << "Add to list (a) \n" <<
+	"Done list item (d)\n" << "Print list (p) \n" <<
+	"Exit list app (x) \n" << "What do you want to do?";
+	
+	
+	
+	return 0;
+}
