fdb2d72d52bd8c2f1c06666c038793f5b0c536a5
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-05-01 18:45:35 -0500

    add while, switch and case

--- main.cpp
+++ main.cpp
@@ -4,10 +4,46 @@
 using namespace std;
 
 int main(){
+	
+	int list = 0;
+	string name;
+	char next = 'x';
+
+	cout << "How long is your list? ";
+	cin >> list;
+	
+while (next != 'x'){
+	
+
+	
 	cout << "\n" << "Add to list (a) \n" <<
 	"Done list item (d)\n" << "Print list (p) \n" <<
 	"Exit list app (x) \n" << "What do you want to do?";
 	
+	switch(next){
+		case 'a':
+			cout << "Name your new todo list: ";
+			cin >> name;
+		break;
+		
+		case 'd':
+		
+		break;
+		
+		case 'p':
+		
+		break;
+		
+		case 'x':
+		
+		break;
+		
+		
+	}
+	
+	
+}
+
 	
 	
 	return 0;
